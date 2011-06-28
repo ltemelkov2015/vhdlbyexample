89dd2557fd1df312313802258ac316508807ae15
lachezar.temelkov@gmail.com <lachezar.temelkov@gmail.com@4f4933e1-6b53-13e8-4dde-d00fa29e2285>
2011-06-28 18:38:03 +0000

    test

--- stopwarch/stop_watch_test.vhd
+++ stopwarch/stop_watch_test.vhd
@@ -7,7 +7,7 @@ end stop_watch_test;
 
 architecture arch of stop_watch_test is
 signal clock: std_logic:='1';
-signal enbl: std_logic:='1';
+signal enbl: std_logic:='0';
 signal clear: std_logic:='1';
 signal digit2, digit1, digit0: std_logic_vector(3 downto 0);
 
@@ -23,11 +23,19 @@ watch_unit: entity work.stop_watch(cascade_arch)
             );
 
   
-control_clear: 
+control_enbl: 
 process
   begin
     wait for 900 ns;
-    enbl<='0';
+    enbl<='1';
+    wait;
+end process control_enbl;
+
+control_clear:
+process
+  begin
+    wait for 650 ns;
+    clear <= '0';
     wait;
 end process control_clear;
 
